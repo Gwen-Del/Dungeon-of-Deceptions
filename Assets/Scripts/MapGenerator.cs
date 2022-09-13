4c416a1145b66cb22e2f79401d94d3728ec213f9
Gwen-Del <gwe.del65@gmail.com>
2022-09-13 21:44:53 +0200

    Début procédural

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public GameObject spawnBase;
-    public GameObject player;
+    public GameObject spawnBasePrefab;
+    public GameObject playerPrefab;
+    public GameObject basicWallPrefab;
 
     void Start()
     {
-        int x = Random.Range(-4,4);
-        int z = Random.Range(-4,4);
+        int BaseX = Random.Range(-4,5);
+        int BaseZ = Random.Range(-4,5);
 
-        Instantiate(spawnBase, new Vector3(x*20,1,z*20), Quaternion.identity);
-        Instantiate(player, new Vector3(x*20,1,z*20), Quaternion.identity);
+        Instantiate(spawnBasePrefab, new Vector3(BaseX*20-10,0,BaseZ*20-10), Quaternion.identity);
+        GameObject player = Instantiate(playerPrefab, new Vector3(BaseX*20-10,0,BaseZ*20-10), Quaternion.identity);
+
+        if(BaseX == -4)
+        {
+            Instantiate(basicWallPrefab, new Vector3(BaseX*20-20,2,BaseZ*20-10), Quaternion.identity).transform.Rotate(0.0f,90.0f,0.0f);
+        }
+        else if(BaseX == 5)
+        {
+            Instantiate(basicWallPrefab, new Vector3(BaseX*20,2,BaseZ*20-10), Quaternion.identity).transform.Rotate(0.0f,90.0f,0.0f);
+        }
+        if(BaseZ == -4)
+        {
+            Instantiate(basicWallPrefab, new Vector3(BaseX*20-10,2,BaseZ*20-20), Quaternion.identity);
+        }
+        else if(BaseZ == 5)
+        {
+            Instantiate(basicWallPrefab, new Vector3(BaseX*20-10,2,BaseZ*20), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
