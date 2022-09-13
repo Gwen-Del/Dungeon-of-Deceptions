c46513fd60e340155990563871b10f7647a0a8b1
Gwen-Del <gwe.del65@gmail.com>
2022-09-13 11:00:24 +0200

    Il le voit

--- Assets/Scripts/Ennemy/EnnemyMovement.cs
+++ Assets/Scripts/Ennemy/EnnemyMovement.cs
@@ -5,27 +5,37 @@ using UnityEngine;
 public class EnnemyMovement : MonoBehaviour
 {
     Transform player;
-    // Start is called before the first frame update
+
+    [Range(0.0f,100.0f)]
+    public float range = 30.0f;
+
+    RaycastHit hit;
+    
     void Start()
     {
         player = GameObject.Find("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        isfront();
+        isVisible();
     }
 
-    bool isfront()
+    bool isVisible()
     {
         Vector3 playerDirection = transform.position - player.position;
         float angle = Vector3.Angle(transform.forward, playerDirection);
+        float distance = Vector3.Distance(transform.position,player.position);
 
-        if(Mathf.Abs(angle) > 90 && Mathf.Abs(angle) < 270)
+        if(Mathf.Abs(angle) > 90 && Mathf.Abs(angle) < 270 && distance <= range)
         {
-            Debug.DrawLine(transform.position, player.position, Color.red);
-            return true;
+            if(Physics.Linecast(transform.position, player.position, out hit)){
+                if(hit.transform.tag == "Player"){
+                    Debug.DrawLine(transform.position, player.position, Color.red);
+                    //c'est ici qu'il le voit
+                    return true;
+                }
+            }
         }
 
         return false;
