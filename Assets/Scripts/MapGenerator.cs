410e5347403f66f9e888b7c8aae0536390c3b15d
Gwen-Del <gwe.del65@gmail.com>
2022-09-13 21:12:29 +0200

    -

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject spawnBase;
+    public GameObject player;
+
     void Start()
     {
-        
+        int x = Random.Range(-4,4);
+        int z = Random.Range(-4,4);
+
+        Instantiate(spawnBase, new Vector3(x*20,1,z*20), Quaternion.identity);
+        Instantiate(player, new Vector3(x*20,1,z*20), Quaternion.identity);
     }
 
     // Update is called once per frame
