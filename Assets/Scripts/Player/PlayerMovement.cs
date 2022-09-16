02ca5b45066d120e1776a17296adf425e08bb2b9
Newlonine <nolwennpaluet@gmail.com>
2022-09-16 09:47:05 +0200

    empty

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     CharacterController characterController;
+    public Image HealthBar;
+    public GameObject Bullet;
 
     public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
+    public float gravity = 1000.0f;
 
     private Vector3 moveDirection = Vector3.zero;
+
+    private const float MAX_HEALTH = 10f;
+    public float health = MAX_HEALTH;
+
+    public float TimeBetweenAttack; 
+    public bool AlreadyAttacked; 
 
     void Start()
     {
@@ -19,19 +28,55 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+         UpdateHealth();
+
         if (characterController.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
             moveDirection *= speed;
-
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
         }
 
         moveDirection.y -= gravity * Time.deltaTime;
-
         characterController.Move(moveDirection * Time.deltaTime);
+
+        if (Input.GetButton("Fire1"))
+        {
+            Attack();
+        }
+    }
+
+    public void UpdateHealth()
+    {
+        HealthBar.fillAmount = health / MAX_HEALTH;
+
+        if(health <= 0){
+           SceneManager.LoadScene("deathMenu", LoadSceneMode.Single);
+        }
+    }
+
+    public void Attack()
+    {
+        if(!AlreadyAttacked){
+
+            Rigidbody rb = Instantiate(Bullet, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+
+            AlreadyAttacked = true;
+            Invoke(nameof(ResetAttack), TimeBetweenAttack);
+        }
+    }
+
+    public void ResetAttack()
+    {
+        AlreadyAttacked = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("bulletEnemy")){
+            health -= 0.5f;
+            // Destroy(other.GetComponent<GameObject>());
+        }
     }
 }
